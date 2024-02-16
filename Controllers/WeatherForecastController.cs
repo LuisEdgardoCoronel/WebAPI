79831423584d688c40bb61896e1d06d3661e9bdb
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-02-15 21:02:27 -0300

    Create Listforecasts
    Cree Listforecasts para almacenar todos los datos en la lista y poder usar los otros metodos

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,21 +13,37 @@ namespace WebAPI.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
+        private static List<WeatherForecast> Listforecasts = new();
+
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
             _logger = logger;
+
+            if(Listforecasts == null || Listforecasts.Any())
+            {
+                Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToList();
+            }
         }
+
+
+
+
 
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Listforecasts;
         }
+
+
+
+
+        [HttpPost]
     }
 }
