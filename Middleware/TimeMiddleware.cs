22a05b149157b36ef41935372d9178e746f1367f
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-02-16 19:45:38 -0300

    Create TimeMiddleware.cs
    Cree un middleware que agrega la hora

---
+++ Middleware/TimeMiddleware.cs
@@ -0,0 +1,42 @@
+﻿namespace WebAPI.Middleware
+{
+    public class TimeMiddleware
+    {
+        readonly RequestDelegate next;
+
+        public TimeMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await next(context);
+
+            if (context.Request.Query.Any(p=>p.Key=="time"))
+            {
+                await context.Response.WriteAsync(DateTime.Now.ToShortTimeString());
+            }
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+        public static class TimeMiddleExtension
+        {
+            public static IApplicationBuilder UseTimeMiddleware(this IApplicationBuilder app)
+            {
+                return app.UseMiddleware<TimeMiddleware>();
+            }
+        }
+}
