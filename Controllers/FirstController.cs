87a2cc642923c8cc4208f4fbed935fb6cb8c5fb2
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-02-16 21:07:24 -0300

    Create FirstController.cs
    controlador para devolver el mensaje del service

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Service;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/[Controller]")]
+    public class FirstController:ControllerBase
+    {
+        IFirstService firstService;
+
+        public FirstController(IFirstService firstService)
+        {
+            this.firstService = firstService;
+        }
+
+        public IActionResult Get()
+        {
+            return Ok(firstService.GetFirst());
+        }
+    }
+}
