c130ec2ab2821124278dd45a0d22c5782910509a
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-03-21 18:31:21 -0300

    create Controllers

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticaEf.Model;
+using WebAPI.Service;
+
+namespace WebAPI.Controllers
+
+{
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        ICategoryService categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(categoryService.Get());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Category category) 
+        {
+            categoryService.Save(category);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, [FromBody] Category category)
+        {
+            categoryService.Update(id, category);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            categoryService.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ Controllers/TasksController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticaEf.Model;
+using WebAPI.Service;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class TasksController: ControllerBase
+    {
+        ITaskService taskService;
+
+        public TasksController(ITaskService taskService)
+        {
+            this.taskService = taskService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(taskService.Get());
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] TaskModel task)
+        {
+            taskService.Save(task);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Guid id, [FromBody] TaskModel task)
+        {
+            taskService.Update(id, task);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            taskService.Delete(id);
+            return Ok();
+        }
+    }
+}
