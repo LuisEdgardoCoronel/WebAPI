f89ed86fd601c6fc3b076557764b828469d88309
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-03-21 16:12:35 -0300

    Create TaskService.cs
    create class TaskService and interface ITaskService

---
+++ Service/TaskService.cs
@@ -0,0 +1,60 @@
+﻿using PracticaEf;
+using PracticaEf.Model;
+
+namespace WebAPI.Service
+{
+    public class TaskService: ITaskService
+    {
+        TaskContext context;
+        public TaskService(TaskContext dbcontext) 
+        {
+            this.context = dbcontext;
+        }
+
+        public IEnumerable<TaskModel> Get()
+        {
+            return context.Tasks;
+        }
+
+        public async Task Save(TaskModel task)
+        {
+            context.Tasks.Add(task);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Update(Guid id, TaskModel task)
+        {
+            var actualTask = context.Tasks.Find(id);
+
+            if (actualTask != null)
+            {
+                actualTask.Title = task.Title;
+                actualTask.Description = task.Description;
+                actualTask.PriorityTask = task.PriorityTask;
+                actualTask.Summary = task.Summary;
+
+                await context.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var actualTask = context.Tasks.Find(id);
+            if (actualTask != null)
+            {
+                context.Remove(actualTask);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+
+
+    public interface ITaskService
+    {
+        IEnumerable<TaskModel> Get();
+        Task Save(TaskModel task);
+        Task Update(Guid id, TaskModel task);
+        Task Delete(Guid id);
+    }
+}
+
