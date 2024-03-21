65190f8e7f771073c1cd30551790ce3aae008655
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-03-21 16:06:47 -0300

    add the Category.cs file
    add the Category.cs file from the “practice EF” project

---
+++ Model/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace PracticaEf.Model
+{
+    public class Category
+    {
+        
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Weight { get; set; }
+        [JsonIgnore]//lo agregue para que no salga error la peticion en postman
+        public virtual ICollection<TaskModel> Tasks { get;}
+    }
+}
