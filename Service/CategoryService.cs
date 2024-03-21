a16364b258163857dd75491715f1b7b517d06c48
LuisEdgardoCoronel <luis.edgardo.coronel@gmail.com>
2024-03-21 16:11:40 -0300

    Create CategoryService.cs
    create class CategoryService and interface ICategoryService

---
+++ Service/CategoryService.cs
@@ -0,0 +1,76 @@
+﻿using PracticaEf;
+using PracticaEf.Model;
+
+namespace WebAPI.Service
+{
+    public class CategoryService:ICategoryService
+    {
+        TaskContext context;
+
+        public CategoryService(TaskContext dbcontext)
+        {
+            this.context = dbcontext;
+        }
+
+        public IEnumerable<Category>Get() 
+        {
+            return context.Categories;
+        }
+
+        public async Task Save(Category category)
+        {
+            context.Categories.Add(category);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task Update(Guid id,Category category)
+        {
+            var actualCategory = context.Categories.Find(id);
+            if (actualCategory != null)
+            {
+                actualCategory.Name = category.Name;
+                actualCategory.Description = category.Description;
+                actualCategory.Weight = category.Weight;
+                
+                await context.SaveChangesAsync();
+            }
+           
+        }
+
+
+        public async Task Delete(Guid id)
+        {
+            var actualCategory = context.Categories.Find(id);
+            if (actualCategory != null)
+            {
+                context.Remove(actualCategory);
+
+                await context.SaveChangesAsync();
+            }
+
+        }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //interfaz para implementar los servicios
+    public interface ICategoryService
+    {
+        IEnumerable<Category> Get();
+        Task Save(Category category);
+        Task Update(Guid id,Category category);
+        Task Delete(Guid id);
+    }
+}
+
+
+
+
+
+
